5c00060404d30a7a340c65e9afe57a782c183f3c
Muhammad Farid Yamin <muhammad.farid.yamin@gmail.com>
2024-03-22 20:12:04 +0700

    Menambahkan favicon, menambahkan waktu pada aside,  memperbaiki arrow

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ window.addEventListener("scroll", () => {
   if (window.scrollY >= 128) arrow.style.opacity = 1;
   else arrow.style.opacity = 0;
 });
+
+const tanggalElement = document.querySelector(".tanggal");
+
+const hariIndonesia = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+const bulanIndonesia = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+
+const sekarang = new Date();
+const hari = hariIndonesia[sekarang.getDay()];
+const tanggal = sekarang.getDate();
+const bulan = bulanIndonesia[sekarang.getMonth()];
+const tahun = sekarang.getFullYear();
+
+tanggalElement.innerHTML = `${hari}, ${tanggal} ${bulan} ${tahun}`;
